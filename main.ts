89ad3bb7f52e51ea349049700f27d6a17339b594
Ray Crist <rayhcrist@gmail.com>
2025-04-30 21:40:52 -0700

    save

--- main.ts
+++ main.ts
@@ -1,85 +1,40 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+	App,
+	Plugin,
+	PluginSettingTab,
+	Setting,
+	TFolder,
+	TFile,
+	Notice,
+	SuggestModal,
+	Editor,
+	MarkdownView,
+  MarkdownFileInfo,
+} from "obsidian";
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface IndexLinkerSettings {
+	defaultIndexName: string;
+	linkTextPattern: string; // e.g., "Index for {folder}"
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+const DEFAULT_SETTINGS: IndexLinkerSettings = {
+	defaultIndexName: "index",
+	linkTextPattern: "{folder}/index",
+};
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class IndexLinkerPlugin extends Plugin {
+	settings: IndexLinkerSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
+			id: "insert-folder-index-link",
+			name: "Insert link to folder index file",
+			editorCallback: (editor, view) => this.openFolderSuggestModal(editor, view),
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
+		this.addSettingTab(new IndexLinkerSettingTab(this.app, this));
 	}
 
 	async loadSettings() {
@@ -89,46 +44,96 @@ export default class MyPlugin extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+
+	openFolderSuggestModal(editor: Editor, view: MarkdownView | MarkdownFileInfo) {
+		const folders = this.app.vault.getAllFolders();
+
+		new FolderSuggestModal(this.app, folders, async (folder) => {
+			const folderName = folder.name;
+			const indexPath = `${folder.path}/${this.settings.defaultIndexName}.md`;
+			let file = this.app.vault.getAbstractFileByPath(indexPath);
+
+			if (!file) {
+				file = await this.app.vault.create(indexPath, `# ${this.settings.defaultIndexName}`);
+				new Notice(`Created file: ${indexPath}`);
+			}
+
+			if (!(file instanceof TFile)) {
+				new Notice(`Error: Could not create or find the index file.`);
+				return;
+			}
+
+			const alias = this.settings.linkTextPattern.replace("{folder}", folderName);
+			const markdownLink = this.app.fileManager.generateMarkdownLink(file, view?.file?.path || "", undefined, alias);
+			editor.replaceSelection(markdownLink);
+		}).open();
+	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
+class FolderSuggestModal extends SuggestModal<TFolder> {
+	folders: TFolder[];
+	onChoose: (folder: TFolder) => void;
+
+	constructor(app: App, folders: TFolder[], onChoose: (folder: TFolder) => void) {
 		super(app);
+		this.folders = folders;
+		this.onChoose = onChoose;
+		this.setPlaceholder("Choose a folder to link its index...");
 	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+	getSuggestions(query: string): TFolder[] {
+		return this.folders.filter((folder) =>
+			folder.path.toLowerCase().contains(query.toLowerCase())
+		);
 	}
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
+	renderSuggestion(folder: TFolder, el: HTMLElement) {
+		el.setText(folder.path);
+	}
+
+	onChooseSuggestion(folder: TFolder) {
+		this.onChoose(folder);
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+class IndexLinkerSettingTab extends PluginSettingTab {
+	plugin: IndexLinkerPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: IndexLinkerPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
-
+		const { containerEl } = this;
 		containerEl.empty();
+		containerEl.createEl("h2", { text: "Index Linker Settings" });
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName("Default Index File Name")
+			.setDesc("The name of the index file to link or create in selected folders (no extension).")
+			.addText((text) =>
+				text
+					.setPlaceholder("index")
+					.setValue(this.plugin.settings.defaultIndexName)
+					.onChange(async (value) => {
+						this.plugin.settings.defaultIndexName = value.trim();
+						await this.plugin.saveSettings();
+					})
+			);
+
+		new Setting(containerEl)
+			.setName("Link Display Text Pattern")
+			.setDesc("Use {folder} to include the folder name in the link alias.")
+			.addText((text) =>
+				text
+					.setPlaceholder("{folder}/index")
+					.setValue(this.plugin.settings.linkTextPattern)
+					.onChange(async (value) => {
+						this.plugin.settings.linkTextPattern = value.trim();
+						await this.plugin.saveSettings();
+					})
+			);
 	}
 }
+
